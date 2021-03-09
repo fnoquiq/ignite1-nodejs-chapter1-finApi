301715478eba769c2067be95104f4dbdd3f69242
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-09 15:46:50 -0300

    :rocket: Cadastro de conta

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+
+  customers.push({
+    id: uuidv4(),
+    cpf,
+    name,
+    statement: []
+  })
+  
+  return response.status(201).send()
+})
+
+app.listen(3333)
